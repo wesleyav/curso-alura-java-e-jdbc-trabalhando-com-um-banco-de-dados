013906c7cfe3c2307e13cf665c244b3b5a928827
Wesley <wesley.contato@hotmail.com>
2020-11-06 19:39:21 -0300

    Modelo e o DAO de categorias

---
+++ loja-virtual-repository/src/TestaListagemDeCategorias.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.modelo.Categoria;
+
+public class TestaListagemDeCategorias {
+
+	public static void main(String[] args) throws SQLException {
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+			List<Categoria> listaDeCategorias = categoriaDAO.listar();
+			listaDeCategorias.stream().forEach(ct -> System.out.println(ct.getNome()));
+		}
+
+	}
+}
