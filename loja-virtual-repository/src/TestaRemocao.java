420183762bec57541ce344babc73f4ed8ccc05ef
Wesley <wesley.contato@hotmail.com>
2020-11-06 06:12:22 -0300

    Listagem e remoção

--- loja-virtual-repository/src/TestaRemocao.java
+++ loja-virtual-repository/src/TestaRemocao.java
@@ -1,14 +1,15 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaRemocao {
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recuperarConexao();
 
-		Statement stm = connection.createStatement();
-		stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+		PreparedStatement stm = connection.prepareStatement("DELETE FROM PRODUTO WHERE ID > ?");
+		stm.setInt(1, 2);
+		stm.execute();
 		
 		Integer linhasModificadas = stm.getUpdateCount();
 		
