3f5def874dc01701a6cb96bf426b8fade748bfba
Wesley <wesley.contato@hotmail.com>
2020-11-06 05:10:18 -0300

    Listagem com Statement

---
+++ loja-virtual-repository/src/TestaListagem.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+
+		Statement stm = connection.createStatement();
+		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		ResultSet rst = stm.getResultSet();
+
+		while (rst.next()) {
+			Integer id = rst.getInt("ID");
+			System.out.println(id);
+			String nome = rst.getString("NOME");
+			System.out.println(nome);
+			String descricao = rst.getString("DESCRICAO");
+			System.out.println(descricao);
+		}
+
+		connection.close();
+	}
+}
